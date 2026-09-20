f3cd395572370a62fd8c829913643aff0515d7fb
SriGowri73 <15gowri05@gmail.com>
2026-09-20 16:44:15 +0530

    Update pgrm1.java

--- CoreJava/src/javabasicpgm/pgrm1.java
+++ CoreJava/src/javabasicpgm/pgrm1.java
@@ -4,14 +4,22 @@ public class pgrm1 {
 
 	public static void main(String[] args) {
 		// patterns printing
-		int n=6;
-		for(int i=n-1;i>=0;i++){
-			for(int j=0;j<=i;j--){
-				System.out.println("* ");
-			}
-			System.out.println();
-		}
-
-	}
-
+		int rows = 9;
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
+output:
+* * * * * * * * * 
+* * * * * * * * 
+* * * * * * * 
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
