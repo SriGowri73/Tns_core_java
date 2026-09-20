202e998daee0aec99359d42a3f3ac11923b268e7
SriGowri73 <15gowri05@gmail.com>
2026-09-20 16:40:33 +0530

    Update pgrm1.java

--- CoreJava/src/javabasicpgm/pgrm1.java
+++ CoreJava/src/javabasicpgm/pgrm1.java
@@ -3,7 +3,14 @@ package com.tns.core.java;
 public class pgrm1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// patterns printing
+		int n=6;
+		for(int i=n-1;i>=0;i++){
+			for(int j=0;j<=i;j--){
+				System.out.println("* ");
+			}
+			System.out.println();
+		}
 
 	}
 
